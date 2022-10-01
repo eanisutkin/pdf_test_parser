d4b682354e9b3ea3e3bf1bc08579df54cd64df19
Evgenii Anisiutkin <eanisutkin@gmail.com>
2022-10-01 15:09:35 +0300

    First parsing variant.

--- main.php
+++ main.php
@@ -9,45 +9,100 @@ if($argc != 2) {
   return -1;
 }
 
-$filename = $argv[1];
+$filepath = $argv[1];
 
-if(!file_exists($filename)) {
-  echo "File dos not exist or the path was specified incorrectly. Please provide a valid filepath.\n";
+if(!file_exists($filepath)) {
+  echo "File does not exist or the path was specified incorrectly. Please provide a valid filepath.\n";
   return -1;
 }
 
-shell_exec("pdftotext -layout test/test.pdf");
-$text_data = file_get_contents("test/test.txt");
+$pathinfo = pathinfo($filepath);
+$path = $pathinfo["dirname"];
+$filename = $pathinfo["filename"];
+$read_path = $path."/".$filename.".txt";
+$result_path = "./json_encoded_result.txt";
 
-$page_number = 0;
-
-$out = fopen("out.txt", "w");
-
-if(!$out) {
-  echo "Coudn't open file for writing\n";
+shell_exec("pdftotext -layout $filepath");
+if(!file_exists($read_path)) {
+  echo "Something went wrong and the file conversion failed. Please try fileconversion outside this script with following command: pdftotext -layout [path to file]\n";
+  return -2;
 }
 
-$re_match = '/Name|Invoice Number|Invoice Date|Invoice Due Date|Invoice Total|Account Number|Job|AWB|CountryName|Service|Format|Pcs|PerPc
-              |PcTotal|Lbs|PerLb|LbTotal|Flat|Pickup|Reference|GrandTotal|[A-Za-z\-]+|[\d\/\.,$]+/';
+$res = Array();
 
-preg_match_all($re_match, $text_data, $match_data, PREG_PATTERN_ORDER, 0);
-$arrays_data = $match_data[0];
-//fwrite($out, print_r($arrays_data, true));
-//exit(0);
+$toRead = fopen($read_path, "r");
 
-$res_array = Array();
+if(!$toRead) {
+  echo "Coudn't open file for reading\n";
+  return -3;
+}
 
-$ind = array_search('Name', $arrays_data);
-if($ind !== false) {
-  $ind2 = array_search('Invoice Number', $arrays_data);
-  $val = "";
-  for($i = $ind+1; $i < $ind2; $i++) {
-    $val .= $arrays_data[$i] . " ";
+$table_found = false;
+$i = 0;
+while( ($line = fgets($toRead)) !== false ) {
+  if(!isset($res["Header"]["Name"]) && count($parts = preg_split("/\bName\b/", $line, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE)) == 2) {
+    $res["Header"]["Name"] = trim($parts[1]);
+    continue;
+  } else if(!isset($res["Invoice Number"]) && count($parts = preg_split("/\bInvoice Number\b/", $line, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE)) == 2) {
+    $res["Header"]["Invoice Number"] = trim($parts[1]);
+    continue;
+  } else if(!isset($res["Header"]["Invoice Date"]) && count($parts = preg_split("/\bInvoice Date\b/", $line, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE)) == 2) {
+    $res["Header"]["Invoice Date"] = trim($parts[1]);
+    continue;
+  } else if(!isset($res["Header"]["Invoice Due Date"]) && count($parts = preg_split("/\bInvoice Due Date\b/", $line, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE)) == 2) {
+    $res["Header"]["Invoice Due Date"] = trim($parts[1]);
+    continue;
+  } else if(!isset($res["Header"]["Invoice Total"]) && count($parts = preg_split("/\bInvoice Total\b/", $line, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE)) == 2) {
+    $res["Header"]["Invoice Total"] = trim($parts[1]);
+    continue;
+  } else if(!isset($res["Header"]["Account Number"]) && count($parts = preg_split("/\bAccount Number\b/", $line, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE)) == 2) {
+    $res["Header"]["Account Number"] = trim($parts[1]);
+    continue;
   }
 
-  $res_array["Header"]["Name"] = trim($val);
-  $res_array["Header"]["Invoice Number"] = $arrays_data[$ind2++];
+  if(count($res["Header"]) != 6) {
+    echo "Header parsed incorrectly. Check parsing or chech file\n";
+    return -4;
+  }
+
+  if(!$table_found && preg_match("/Job/", $line)) {
+    $parts = preg_split("/\s{2,}/", $line, -1, PREG_SPLIT_NO_EMPTY);
+    $table_found = true;
+
+    foreach($parts as $part) {
+      $res["Table"]["$part"] = Array();
+    }
+    continue;
+  } else if(!$table_found) {
+    continue;
+  } else if($table_found && preg_match("/Job/", $line)) {
+    continue;
+  } else if($table_found && $parts = preg_split("/\s{2,}/", $line, -1, PREG_SPLIT_NO_EMPTY)) {
+    if(count($parts) == 14 || count($parts) == 15) {
+      $j = 0;
+      foreach($res["Table"] as $key => $value) {
+        if(count($parts) == 14 && $key == "Reference") {
+          $res["Table"]["Reference"]["$i"] = "";
+          continue;
+        }
+        $res["Table"]["$key"]["$i"] = $parts[$j];
+        $j++;
+      }
+      $i++;
+    }
+    continue;
+  } else {
+    continue;
+  }
 }
 
-fwrite($out, json_encode($res_array));
+fclose($toRead);
 
+$out = fopen($result_path, "w");
+if(!$out) {
+  echo "Couldn't open result file for writing\n";
+  return - 5;
+}
+
+fwrite($out, json_encode($res));
+fclose($out);
